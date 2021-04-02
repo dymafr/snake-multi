f074306c1e331aa35c33e3d116f01e694b204cce
dymafr <contact@dyma.fr>
2021-04-02 11:29:40 +0200

    maj

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
-const Player = require("./Player.js");
-const Game = require("./Game.js");
-const { v4: uuidv4 } = require("uuid");
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const Player = require('./Player.js');
+const Game = require('./Game.js');
+const { v4: uuidv4 } = require('uuid');
 let waitingPlayers = [];
 
 if (process.env.PORT) {
@@ -14,26 +14,26 @@ if (process.env.PORT) {
   http.listen(80);
 }
 
-app.use(express.static(path.join(__dirname, "../public")));
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "../public/index.html"))
+app.use(express.static(path.join(__dirname, '../public')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '../public/index.html'))
 );
 
-io.on("connection", (socket) => {
+io.on('connection', (socket) => {
   const player = new Player(socket.id);
   let game;
   waitingPlayers.push(player);
 
   if (waitingPlayers.length === 2) {
     const gameRoom = uuidv4();
-    io.sockets.connected[waitingPlayers[0].id].join(gameRoom);
-    io.sockets.connected[waitingPlayers[1].id].join(gameRoom);
+    io.of('/').sockets.get(waitingPlayers[0].id).join(gameRoom);
+    io.of('/').sockets.get(waitingPlayers[1].id).join(gameRoom);
     game = new Game(waitingPlayers, gameRoom, io);
     game.initGame();
     waitingPlayers = [];
   }
 
-  socket.on("disconnect", () => {
+  socket.on('disconnect', () => {
     waitingPlayers = waitingPlayers.filter((p) => p.id !== socket.id);
     if (game) {
       game.players = game.players.filter((p) => p.id !== socket.id);
@@ -43,21 +43,21 @@ io.on("connection", (socket) => {
     }
   });
 
-  socket.on("move", (arrow) => {
+  socket.on('move', (arrow) => {
     switch (arrow) {
-      case "ArrowUp":
+      case 'ArrowUp':
         player.velociteHorizontale = 0;
         player.velociteVerticale = -1;
         break;
-      case "ArrowDown":
+      case 'ArrowDown':
         player.velociteHorizontale = 0;
         player.velociteVerticale = 1;
         break;
-      case "ArrowRight":
+      case 'ArrowRight':
         player.velociteHorizontale = 1;
         player.velociteVerticale = 0;
         break;
-      case "ArrowLeft":
+      case 'ArrowLeft':
         player.velociteHorizontale = -1;
         player.velociteVerticale = 0;
         break;
